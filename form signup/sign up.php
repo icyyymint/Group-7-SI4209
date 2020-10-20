e90e696ece4dce09494fdc45b088d59f01a4a6eb
alifrafikurnia <alifrafi0304@gmail.com>
2020-10-20 19:00:53 +0700

    add form signup

---
+++ form signup/sign up.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <a class="navbar-brand" href="#">
+    <img src="logo/tcr transparent.png" class="img-fluid" width="150" height="100" alt="">
+   </a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+     <div class="navbar-nav">
+       <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+       <a class="nav-link" href="#">About Us</a>
+       <a class="nav-link" href="#">Donate</a>
+       <a class="nav-link" href="#">Blog</a>
+       <a class="nav-link" href="#">Contact</a>
+     </div>
+     <div class="form-inline ml-auto">
+      <button class="btn btn-outline-danger mr-3" type="button">Login</button>
+      <button class="btn btn-danger" type="button">Sign Up</button>
+     </div>
+   </div>
+ </nav>
+      <div class="row">
+       <div class="col-md-3" >
+       	<img src="logo/tcr.png" style="width:415%; height: 500px">
+     </div>
+    </iframe>
+    
+
+
+      <div class="col-md-6" style= "background-color: #ffdbcc" >
+       <br>
+       
+       <form>
+       	<h5 class="text-center"> Sign Up </h5>
+       <br>
+      
+      <div class="form-group mx-sm-5 mb-2">
+    <label for="exampleInputEmail1">Username</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username">
+  </div>
+  	<div class="form-group mx-sm-5 mb-2">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group mx-sm-5 mb-2">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group mx-sm-5 mb-2">
+    <label for="exampleInputPassword1">Confirm Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password">
+  </div>
+  <div class="form-group mx-sm-5 mb-2">
+    <label for="exampleInputEmail1">Phone Number</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter phone number">
+  </div>
+
+    <div class="form-group row">
+        <label for="inputAlamat" class="col-sm-5 col-form-label"></label>
+        <div class="col-sm-6">
+          <button class="btn btn-danger text-center">SIGN UP</button>
+        </div>
+      </div>
+      
+    </form>
+       
+      </div>
+      <div class="col-md-3">
+      	
+      </div>
+    </div>
+
+</body>
+</html>
