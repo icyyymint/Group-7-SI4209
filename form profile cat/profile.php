c7e48c3b8f3769460f3aaf69344e58a069bf8fb5
Tita Arum Parawasi <titaarumparawasi@gmail.com>
2020-10-20 19:30:42 +0700

    push form profile cat

---
+++ form profile cat/profile.php
@@ -0,0 +1,85 @@
+<head>
+    <title>Form Input Data dan Profile</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href= "background.css">
+</head>
+<body>
+</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <a class="navbar-brand" href="#">
+    <img src="logo.png" class="img-fluid" width="150" height="100" alt="">
+   </a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+     <div class="navbar-nav">
+       <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+       <a class="nav-link" href="#">About Us</a>
+       <a class="nav-link" href="#">Donate</a>
+       <a class="nav-link" href="#">Blog</a>
+       <a class="nav-link" href="#">Contact</a>
+     </div>
+     <div class="form-inline ml-auto">
+      <button class="btn btn-outline-danger mr-3" type="button">Login</button>
+      <button class="btn btn-danger" type="button">Sign Up</button>
+     </div>
+   </div>
+ </nav>
+<div class="row">
+       <div class="col-md-3" >
+        <img src="tcr.png" style="width:315%; height: 300px">
+     </div>
+    </iframe>
+    
+
+
+      <div class="col-md-6" style= "background-color: #ffdbcc" >
+       <br>
+       
+       <form>
+        <h5 class="text-center"> Profile </h5>
+       <br>
+      
+      <div class="form-group mx-sm-5 mb-2">
+    <label for="exampleInputEmail1">Nama</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+  </div>
+    <div class="form-group mx-sm-5 mb-2">
+    <div class="form-group">
+    <label for="exampleFormControlTextarea1">Social Media</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+  </div>
+  <div class="form-group mx-sm-5 mb-2">
+    <div class="form-group">
+    <label for="exampleFormControlTextarea1">Biodata Kucing</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+  </div>
+  <div class="form-group mx-sm-5 mb-2">
+<div class="form-group">
+    <label for="exampleFormControlTextarea1">Hewan Peliharaan</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+  </div>
+  <div class="form-group mx-sm-5 mb-2">
+    <form>
+  <div class="form-group">
+    <label for="exampleFormControlFile1">Foto Kucing</label>
+    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+  </form>
+  </div>
+    <div class="col-auto my-1">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+  </div>
+</form>
+</body>
+</html>
